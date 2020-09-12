36e5c570ccee721d4f43759cbbec1f4ecf05cac2
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-12 17:28:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,11 +41,11 @@ class Song
     self.all.sort_by{|song| song.name }
   end 
   
-  def self.new_from_filename 
-    
+  def self.new_from_filename(filename)
+    parts = 
   end 
   
-  def self.create_from_filename 
+  def self.create_from_filename(filename) 
     
   end 
   
